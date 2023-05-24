b5924a6748f8bddc97d84b02a77b75fa61c1374f
luis10barbo <luis10barbo2020@gmail.com>
2023-05-24 10:55:40 -0300

    Added editable text

--- src/components/goals/card/GoalCardStep.tsx
+++ src/components/goals/card/GoalCardStep.tsx
@@ -1,11 +1,19 @@
 import { Goal } from "./GoalCard";
+import { EditableText } from "./GoalEditableComponents";
 
 export type GoalStep = Goal["goalSteps"][0];
-const GoalCardSteps: React.FC<{ step: GoalStep }> = ({ step }) => {
+const GoalCardSteps: React.FC<{ step: GoalStep; isEditing: boolean }> = ({
+  step,
+  isEditing,
+}) => {
   return (
     <div className="bg-neutral-500/20 p-2 rounded-md">
       <div className={`step-${step.idGoalStep}`} key={step.idGoalStep}>
-        <p className="text-sm text-neutral-300">{step.contentStep}</p>
+        <EditableText
+          className="text-sm text-neutral-300"
+          textValue={step.contentStep}
+          isEditing={isEditing}
+        />
       </div>
     </div>
   );

31e9200ed30202e7700a66d879d472813fda81be
luis10barbo <luis10barbo2020@gmail.com>
2023-05-23 22:29:21 -0300

    Fixed goalsTable functions

--- src/app/server/db/goalsTable.ts
+++ src/app/server/db/goalsTable.ts
@@ -4,6 +4,7 @@ import db from "@/app/server/prisma";
 import { Goal } from "@prisma/client";
 import { getUser } from "../session/user";
 import { AuthenticatedCall } from "../templates/callTemplates";
+import { revalidatePath } from "next/cache";
 
 export async function getGoal(idGoal: Goal["idGoal"]) {
   return db.goal.findUnique({
@@ -23,6 +24,10 @@ export async function saveGoal(params: {
   nameGoal: string;
   descriptionGoal?: string;
   goalSteps: string[];
+  reward: {
+    nameReward: string;
+    valueReward: number;
+  };
 }) {
   new AuthenticatedCall().execute(async ({ user }) => {
     await db.goal.create({
@@ -30,6 +35,17 @@ export async function saveGoal(params: {
         completed: false,
         nameGoal: params.nameGoal,
         descriptionGoal: params.descriptionGoal,
+        reward: {
+          create: {
+            nameReward: params.reward.nameReward,
+            valueReward: params.reward.valueReward,
+            user: {
+              connect: {
+                idUser: user.idUser,
+              },
+            },
+          },
+        },
         goalSteps: {
           create: params.goalSteps.map((step) => {
             return { completed: false, contentStep: step };

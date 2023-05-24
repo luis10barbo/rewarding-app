bf60889f9469f18642977ab3e38e7d69b682e3fa
luis10barbo <luis10barbo2020@gmail.com>
2023-05-23 22:49:52 -0300

    Create Home Component

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -4,14 +4,14 @@ import {
   saveGoal,
   deleteAllGoals,
 } from "@/app/server/db/goalsTable";
-import { register, login, logout } from "@/app/server/session/user";
+import { logoutServer } from "@/app/server/session/user";
 import { UserContext } from "@/context/UserContext";
 import { useState, useEffect, useContext } from "react";
 
 const Home: React.FC = () => {
   const [goals, setGoals] = useState<AllGoals>();
 
-  const { user } = useContext(UserContext);
+  const { user, login, register } = useContext(UserContext);
 
   useEffect(() => {
     getAllGoals().then((goal) => setGoals(goal));
@@ -43,7 +43,7 @@ const Home: React.FC = () => {
         ) : (
           <button
             onClick={() => {
-              logout();
+              logoutServer();
             }}
           >
             logout
@@ -57,6 +57,7 @@ const Home: React.FC = () => {
               goalSteps: ["wAGA", "Outra etapa"],
               descriptionGoal:
                 "A descricao mais pica do mundo e maior mais logan longa logan",
+              reward: { nameReward: "Pizza", valueReward: 72.5 },
             });
           }}
         >
@@ -79,16 +80,44 @@ const Home: React.FC = () => {
           return (
             <div
               key={goal.idGoal}
-              className="flex gap-2 w-[512px] min-h-[6rem] bg-gradient-to-tr from-neutral-900 to-neutral-700 p-4 text-white rounded-xl"
+              className="flex gap-2 w-[512px] min-h-[6rem] bg-gradient-to-tr bg-neutral-500/20  p-4 text-white rounded-xl"
             >
-              <div className="flex flex-col flex-1">
-                <p>{goal.nameGoal}</p>
-                <p className="text-neutral-300 text-sm mb-4">
-                  {goal.descriptionGoal}
-                </p>
+              <div className="flex flex-col flex-1 gap-2">
+                <div className="flex gap-4">
+                  <div className="flex flex-col">
+                    <p className="text-xl">{goal.nameGoal}</p>
+                    <p className="text-neutral-300 text-sm">
+                      {goal.descriptionGoal}
+                    </p>
+
+                    {goal.goalSteps.length > 0 && (
+                      <p className="text-sm mt-auto">Etapas</p>
+                    )}
+                  </div>
+
+                  <div>
+                    {goal.reward && (
+                      <div className="flex flex-col text-center gap-2">
+                        <p className="text-sm ">Recompensa</p>
+                        <div className="aspect-square relative  h-24 bg-neutral-600/30 rounded-md">
+                          <div className="absolute w-full h-full justify-center items-center flex flex-col ">
+                            {goal.reward.nameReward && (
+                              <p>{goal.reward.nameReward}</p>
+                            )}
+                            {goal.reward.valueReward && (
+                              <p className="text-sm text-neutral-400">
+                                (R${goal.reward.valueReward})
+                              </p>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+
                 {goal.goalSteps.length > 0 && (
-                  <div className="goal-steps mt-auto flex flex-col gap-2 ">
-                    <p className="text-sm">Etapas</p>
+                  <div className="goal-steps flex flex-col gap-2 ">
                     {goal.goalSteps.map((step, index) => {
                       return (
                         <div
@@ -108,23 +137,21 @@ const Home: React.FC = () => {
                     })}
                   </div>
                 )}
-              </div>
-              {goal.reward && (
-                <div className="flex flex-col text-center gap-2">
-                  <p className="text-sm ">Recompensa</p>
-                  <div className="aspect-square relative  h-24 bg-neutral-600/30 rounded-md">
-                    <div className="absolute w-full h-full justify-evenly items-center flex flex-col ">
-                      {goal.reward.nameReward && (
-                        <p>{goal.reward.nameReward}</p>
-                      )}
-                      <div className="h-[1px] w-full bg-neutral-500/20"></div>
-                      {goal.reward.valueReward && (
-                        <p>R${goal.reward.valueReward}</p>
-                      )}
+                {/* {goal.reward && (
+                  <>
+                    <p className="text-sm">Recompensas</p>
+                    <div className="flex gap-2 items-center">
+                      <div className="p-2 bg-neutral-500/20 w-fit rounded-md">
+                        {goal.reward.nameReward}
+                      </div>
+                      ou
+                      <div className="p-2 bg-neutral-500/20 w-fit rounded-md">
+                        R${goal.reward.valueReward.toString().replace(".", ",")}
+                      </div>
                     </div>
-                  </div>
-                </div>
-              )}
+                  </>
+                )} */}
+              </div>
             </div>
           );
         })}

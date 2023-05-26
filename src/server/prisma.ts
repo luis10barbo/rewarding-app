1190cfd453ccd556e2747fd135db0b80a9d02dcb
luis10barbo <luis10barbo2020@gmail.com>
2023-05-26 08:59:42 -0300

    Added prisma logging on development

--- src/server/prisma.ts
+++ src/server/prisma.ts
@@ -3,7 +3,13 @@ import { PrismaClient } from "@prisma/client";
 const globalPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-export const db = globalPrisma.prisma ?? new PrismaClient();
+export const db =
+  globalPrisma.prisma ??
+  new PrismaClient({
+    ...(process.env.NODE_ENV === "development" && {
+      log: ["query", "info", "warn", "error"],
+    }),
+  });
 
 if (process.env.NODE_ENV !== "production") globalPrisma.prisma = db;
 

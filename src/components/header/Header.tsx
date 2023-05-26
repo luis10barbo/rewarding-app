f9c33e98e71765e5316708b0edc8ee1c63876527
luis10barbo <luis10barbo2020@gmail.com>
2023-05-26 09:14:09 -0300

    Modified header styling

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,33 @@
-import { logoutServer } from "@/server/session/user";
 import HeaderButton from "./HeaderButton";
 import { useContext } from "react";
 import { UserContext } from "@/context/UserContext";
+import Link from "next/link";
 
 const Header: React.FC<{ extraComponents?: React.ReactNode }> = ({
   extraComponents = <></>,
 }) => {
   const { register, login, user, logout } = useContext(UserContext);
   return (
-    <header className="flex justify-center items-center gap-2">
+    <header className=" overflow-y-auto flex max-w-[100vw]  items-center gap-2 pb-4">
+      <Link href={"/"}>
+        <HeaderButton
+          onClick={async () => {
+            await login("luis10barbo", "teste");
+          }}
+        >
+          Goals
+        </HeaderButton>
+      </Link>
+      <Link href={"/me/rewards"}>
+        <HeaderButton
+          onClick={async () => {
+            await login("luis10barbo", "teste");
+          }}
+        >
+          Rewards
+        </HeaderButton>
+      </Link>
       {extraComponents}
-
       {!user ? (
         <>
           <HeaderButton

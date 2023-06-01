d3ed89c778086a8cb080ef3fdb78c07e1e7b9f58
luis10barbo <luis10barbo2020@gmail.com>
2023-06-01 08:34:16 -0300

    Added goals provider to main me page

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1,8 +1,13 @@
 "use client";
 import { NextPage } from "next";
 import GoalsPage from "./goals/page";
+import { GoalsProvider } from "@/context/GoalsContext";
 
 const MePage: NextPage = () => {
-  return <GoalsPage />;
+  return (
+    <GoalsProvider>
+      <GoalsPage />
+    </GoalsProvider>
+  );
 };
 export default MePage;

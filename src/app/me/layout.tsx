9d853133238a1b5183243ec4fd3d46b699d2286f
luis10barbo <luis10barbo2020@gmail.com>
2023-06-03 15:24:59 -0300

    Only allow access if user is logged in

--- src/app/me/layout.tsx
+++ src/app/me/layout.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { UserContext } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+
 export default function MeLayout({ children }: { children: React.ReactNode }) {
-  return <div className="/me py-4 w-full flex flex-col">{children}</div>;
+  const { user, isLoadingUser, loadUser } = useContext(UserContext);
+  const navRouter = useRouter();
+  async function tryLoadingUser() {
+    const result = await loadUser();
+    console.log(result);
+    if (!result) navRouter.push("/login");
+  }
+  useEffect(() => {
+    if (user === null && !isLoadingUser) {
+      tryLoadingUser();
+    }
+  }, [user]);
+  return (
+    <div className="/me py-4 w-full flex flex-col">
+      {isLoadingUser ? <p>Loading user</p> : children}
+    </div>
+  );
 }
